315f96410b0c49647bb320b5fb6f9a97f1d03b22
Patrykshub <63500126+Patrykshub@users.noreply.github.com>
2020-04-26 14:42:38 +0100

    pig game 03, active panel and player change

--- starter/app.js
+++ starter/app.js
@@ -15,15 +15,6 @@ var scores, roundScore, avtivePlayer;
 scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
-//dice = Math.floor(Math.random() * 6) + 1 // generuje randomowo cyfre pomiedzy 1 a 6.
-//    console.log(dice);
-
-//document.querySelector('#current-' + activePlayer).textContent = dice;
-
-// document.querySelector('#current-' + activePlayer).innerHTML = '<em> dice </em>';
-
-//var x = document.querySelector('#score-0').textContent;
-//console.log(x);
 
 document.querySelector('.dice').style.display = 'none';
 /*
@@ -52,15 +43,52 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
         diceDOM.style.display = 'block';
         diceDOM.src = 'dice-' + dice + '.png';
 
-
         // Update the score IF the rolled number is NOT a 1.
+        if(dice > 1){
+            // add score
+            roundScore += dice;
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
 
+        } else {
+            // next player
+            activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+
+            roundScore = 0;
+
+            document.querySelector('#current-0').textContent = '0'; 
+            document.querySelector('#current-1').textContent = '0'; 
+
+            //document.querySelector('.player-0-panel').classList.remove('active');
+            //document.querySelector('.player-1-panel').classList.add('active');
+            document.querySelector('.player-0-panel').classList.toggle('active');
+            document.querySelector('.player-1-panel').classList.toggle('active');
+
+            document.querySelector('.dice').style.display = 'none';
+            
+
+            /*
+            if(activePlayer === 0){
+                activePlayer = 1;
+            }else{
+                activePlayer = 0;
+            }
+            */
+        }
 
 
 });
 
 
 
+//dice = Math.floor(Math.random() * 6) + 1 // generuje randomowo cyfre pomiedzy 1 a 6.
+//    console.log(dice);
+
+//document.querySelector('#current-' + activePlayer).textContent = dice;
+
+// document.querySelector('#current-' + activePlayer).innerHTML = '<em> dice </em>';
+
+//var x = document.querySelector('#score-0').textContent;
+//console.log(x);
 
 
 
