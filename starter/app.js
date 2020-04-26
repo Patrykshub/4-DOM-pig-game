841400ce07e550e515f1dfd4f2b3f4e5f5e64c59
Patrykshub <63500126+Patrykshub@users.noreply.github.com>
2020-04-26 13:16:04 +0100

    pig game 03 events

--- starter/app.js
+++ starter/app.js
@@ -16,14 +16,14 @@ scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
 //dice = Math.floor(Math.random() * 6) + 1 // generuje randomowo cyfre pomiedzy 1 a 6.
-    console.log(dice);
+//    console.log(dice);
 
-document.querySelector('#current-' + activePlayer).textContent = dice;
+//document.querySelector('#current-' + activePlayer).textContent = dice;
 
 // document.querySelector('#current-' + activePlayer).innerHTML = '<em> dice </em>';
 
-var x = document.querySelector('#score-0').textContent;
-console.log(x);
+//var x = document.querySelector('#score-0').textContent;
+//console.log(x);
 
 document.querySelector('.dice').style.display = 'none';
 /*
@@ -33,13 +33,24 @@ function btn(){
 btn();
 */
 
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
-document.querySelector('btn-roll').addEventListener('click', function(){
+
+
+
+document.querySelector('.btn-roll').addEventListener('click', function(){
 
         // Random number
          var dice = Math.floor(Math.random() * 6) + 1
 
         // Display the result
+        diceDOM = document.querySelector('.dice');
+
+        diceDOM.style.display = 'block';
+        diceDOM.src = 'dice-' + dice + '.png';
 
 
         // Update the score IF the rolled number is NOT a 1.
